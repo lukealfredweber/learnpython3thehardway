2256302f6faea407d89451201ce3f0a773d33fad
lukealfredweber <lukealfredweber@gmail.com>
2018-04-22 21:10:27 +1000

    ex12, 13

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# prompt user and input result into variables
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+# print format string with embedded variables
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
---
+++ ex13.py
@@ -0,0 +1,9 @@
+# add import feature to program
+from sys import argv
+# unpack argv variable into variables
+script, first, second, third = argv
+# print strings / variables
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
