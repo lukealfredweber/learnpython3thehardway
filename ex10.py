a1329490bb1c4115a03485641e51fd31463cd28e
lukealfredweber <lukealfredweber@gmail.com>
2018-04-22 14:36:53 +1000

    ex4 update

---
+++ ex10.py
@@ -0,0 +1,17 @@
+# define and initialise variables including escape sequences
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# print strings
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
---
+++ ex11.py
@@ -0,0 +1,14 @@
+# print prompt to user, don't end line
+print("How old are you?", end=' ')
+# read input from user into variable
+age = input()
+# print prompt to user, don't end line
+print("How tall are you?", end=' ')
+# read input from user into variable
+height = input()
+# print prompt to user, don't end line
+print("How much do you weigh?", end=' ')
+# read input from user into variable
+weight = input()
+# print string with embedded variables
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
